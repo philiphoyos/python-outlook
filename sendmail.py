2c96fa04b44fb1cf864df814ea3887ad29745e0f
Philip <philiphoyos@gmail.com>
2016-06-19 21:05:15 +0200

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -1,13 +1,14 @@
+#
 import win32com.client
 olMailItem = 0x0
 obj = win32com.client.Dispatch("Outlook.Application")
 newMail = obj.CreateItem(olMailItem)
-newMail.Subject = "Python genereret mail"
+newMail.Subject = "Python generated mail"
 #newMail.HTMLBody  = htmltext
 newMail.Body = """
 				Test mail fra Python
 				"""
-newMail.To = "ph@inspari.dk"
+newMail.To = "yourmail@domain.com"
 #attachment1 = "c:\\mypic.jpg"
 #newMail.Attachments.Add(attachment1)
 newMail.Send()
